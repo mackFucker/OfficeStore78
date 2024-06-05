c512f9734f45d8243b09c8aebf21250467827e4c
mackFucker <104640406+mackFucker@users.noreply.github.com>
2024-06-05 10:01:52 +0300

    admin panel functionality fixes

--- college-project-master/WpfApp1/Wndws/MainWindow.xaml.cs
+++ college-project-master/WpfApp1/Wndws/MainWindow.xaml.cs
@@ -6,11 +6,15 @@ namespace WpfApp1
     public partial class MainWindow : Window
     {
         private readonly ProductService _productService;
+        private readonly UserService _userService;
 
         public MainWindow()
         {
             InitializeComponent();
             _productService = new ProductService();
+            _userService = new UserService();
+            ConfigureUIBasedOnRole();
+
         }
 
         private void AddProductButton_Click(object sender, RoutedEventArgs e)
@@ -30,5 +34,26 @@ namespace WpfApp1
             var cartWindow = new CartWindow(_productService);
             cartWindow.Show();
         }
+
+        private void ConfigureUIBasedOnRole()
+        {
+            if (UserService.LoggedInUserRole == "Admin")
+            {
+                // Show Admin button or automatically open AdminWindow
+                AdminButton.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                AdminButton.Visibility = Visibility.Collapsed;
+                AddProductButton.IsEnabled = UserService.LoggedInUserRole == "Manager";
+            }
+        }
+
+        private void AdminButton_Click(object sender, RoutedEventArgs e)
+        {
+            AdminWindow adminWindow = new AdminWindow(_userService);
+            adminWindow.ShowDialog();
+        }
+
     }
 }
--- college-project-master/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ college-project-master/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9f8e5c9389737578a8816c0a48a6979fe22bcf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d3b2fe3498cf6440f4d9db2fe7eab4a71bcac99")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
